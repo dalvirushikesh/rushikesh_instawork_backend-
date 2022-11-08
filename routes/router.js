d2404dccfe859b7495a112a874b22831c6879d1e
Rushikesh Dalvi <dalvi.ru@northeastern.edu>
2022-11-08 18:21:18 -0500

    updating data achieved

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,11 @@
-
 //Package Imports
 const express = require("express");
 const con = express.Router();
 const {User} = require("../models");
  User.sequelize.sync();
+
+
+
 
  //api endpoint to check app health
 con.get("/healthz", (req, res) => {
@@ -22,13 +24,10 @@ con.get("/healthz", (req, res) => {
       role: req.body.role
 
     });
-      
-   
-        return res.status(201).send(newuser);
+      return res.status(201).send(newuser); //201 is the status code for created new team member 
       }
       catch(err) {
-      
-          return res.status(400).send(err);
+          return res.status(400).send(err); //400 is the status code for bad request
         }
   });
 
@@ -36,34 +35,76 @@ con.get("/healthz", (req, res) => {
 //get all team members end point with sequelize
 con.get("/v1/account", async (req, res) => {
     try{
-     const team_members = await User.findAll({
-     
-    });
-              return res.status(200).send(team_members);
-}
+      const team_members = await User.findAll({   //find all users
+
+      });
+      return res.status(200).send(team_members); //200 is the status code for ok
+    }
       catch(err) {
-        
-          console.log(err);
-          return res.status(400).send("Bad Request");
+      console.log(err);
+      return res.status(400).send("Bad Request"); //400 is the status code for bad request
       }
       });
   
+
+
+//update team member end point with sequelize
+con.put("/v1/account/:id", async (req, res) => {
+  // Checking  any other fields than the editable fields
+  try{
+  const bodyfields = req.body;    //get the body fields
+  for (let x in bodyfields) {    //loop through the body fields
+    if (
+      x != "first_name" &&
+      x != "last_name" &&
+      x != "email" &&
+      x != "phone_number" &&
+      x != "role"
+    ) {
+      return res.status(400).send("Bad Request"); //400 is the status code for bad request
+    }
+  }
+            const Accu = await User.update({      //update the user
+              first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                phone_number: req.body.phone_number,
+                role: req.body.role},
+              {
+                where: {
+                  id:req.params.id, 
+                },
+            });
+              return res.status(200).send(dbAcc); //200 is the status code for ok
+    }
+    catch(err)  {
+        console.log(err);
+        return res.status(400).send("Bad Request"); //400 is the status code for bad request
+      }
+    });
+
   //delete team member data with sequalize 
 con.delete("/v1/account/:id", async (req, res) => {
     try{
-    const team_member = await User.destroy({
+      const userr = await User.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (userr) {
+    const team_member = await User.destroy({ //delete the user
       where: {
         id: req.params.id,
       },
-      
     });
-    res.sendStatus(204);
-      
-      }
-      catch(err) {
-        
-          console.log(err);
-          return res.status(400).send("Bad Request");
+    res.sendStatus(204);}
+    else
+    {
+      res.status(404).send("Not Found"); //404 is the status code for not found
+    }}
+    catch(err) {
+        console.log(err);
+        return res.status(400).send("Bad Request");
       }
       });
 
